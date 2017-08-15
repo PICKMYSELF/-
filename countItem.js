a0f01e4e37f03bb172621003ed238c53540f921c
PICKMYSELF <78474978@qq.com>
2017-08-15 14:29:02 +0800

    '2017-8-15'

---
+++ countItem.js
@@ -0,0 +1,8 @@
+
+var  countItem=(arr,item)=>{
+    return arr.filter(element=> element!=item);
+}
+let arr = [1, 3, 7, 1 ,4,];
+let item = 1;
+console.log(`数组{${arr}}去掉${item}后为`);
+console.log(countItem(arr,item));
