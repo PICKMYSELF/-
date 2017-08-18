2c489ef8b012fb120f8b6a997332650f98344632
PICKMYSELF <78474978@qq.com>
2017-08-18 14:25:04 +0800

    8-18

---
+++ itemIndexFinder.js
@@ -0,0 +1,22 @@
+/*
+在数组 arr 中，查找值与 item 相等的元素出现的所有位置
+例如：arr = [1, 3, 7, 1 ,4,] , item = 1，输出：0, 3
+要求：不要使用for，while循环*/
+var IndexFinder=(arr,element)=>{
+    let result=[];
+    arr.filter((item,index)=>item==element&&result.push(index));
+    return result;
+
+}
+
+/*
+function findAllOccurrences(arr, target) {
+    var result=[];
+    arr.filter(function(item,index){
+        return item===target&&result.push(index);
+    });
+    return result;
+}*/
+let arr=[1, 3, 7, 1 ,4,];
+let  item=1;
+console.log(`数组[${arr}]中元素${item}的位置是${IndexFinder(arr,item)}`);
